b406dc4a8ff3c5cc4720f41dead2e7de78ad31a2
lakith <lakith1995@gmail.com>
2019-03-23 18:08:25 +0530

    settin eureka-naming server

--- itemService/src/main/java/com/itemsharing/cloudConfig/itemService/model/User.java
+++ itemService/src/main/java/com/itemsharing/cloudConfig/itemService/model/User.java
@@ -2,15 +2,14 @@ package com.itemsharing.cloudConfig.itemService.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+
 
 import javax.persistence.*;
 import java.util.*;
 
 @Entity
 @Table(name = "user")
-public class User implements UserDetails {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -100,33 +99,6 @@ public class User implements UserDetails {
 
     public void setUserRoleList(List<UserRole> userRoleList) {
         this.userRoleList = userRoleList;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        Set<GrantedAuthority> authorities = new HashSet<>();
-        userRoleList.forEach(userRole -> authorities.add(new Authority(userRole.getRole().getRoleName())));
-        return authorities;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
     }
 
 }
